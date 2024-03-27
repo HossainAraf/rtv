45980a3b65af22ef2703bc053a9fb5a51e4987ae
Hossain <arafat.kd99@gmail.com>
2024-03-27 21:08:40 +0100

    Add route for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
+  # get 'dashboard/index'
   resources :feedbacks
   resources :infos
   get 'home/index'
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
 
+  authentcated :user do
+    root 'dashboard#index', as: 'authenticated_root'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
